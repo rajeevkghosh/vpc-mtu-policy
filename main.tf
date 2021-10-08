6786577f7dcf1591a09097298d199b998d90809a
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-10-08 14:07:35 +0530

    commit-2

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 provider "google" {
-    credentials = file("./key.json")
+    #credentials = file("./key.json")
+    access_token = var.access_token
+    project = "tokyo-nomad-323506"
 
 }
 
-resource "google_compute_network" "vpc_network" {
+resource "google_compute_network" "vpc_network2" {
   project                 = "tokyo-nomad-323506"
-  name                    = "vpc-network"
+  name                    = "vpc-network2"
   auto_create_subnetworks = true
   mtu                     = 1460
 }
